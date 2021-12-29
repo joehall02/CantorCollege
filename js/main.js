c436271bcc8328edfba8bc30b29d92955af7d889
joehall02 <joehall0207@gmail.com>
2021-12-29 21:45:52 +0000

    javascript

--- js/main.js
+++ js/main.js
@@ -2,7 +2,13 @@ $(document).ready(function () {
   // light slider config
   $("#lightSlider").lightSlider({
     item: 1,
-    slideMargin: 0,
-    loop: true, //not working
+    slideMargin: 5,
+    loop: true,
+    mode: "slide",
+    auto: true,
+    speed: 3000,
+    pauseOnHover: true,
+    pause: 5000,
+    controls: true, //not visable
   });
 });
